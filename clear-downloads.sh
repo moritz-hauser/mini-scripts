e327a4dfe21148a9ab07b1b5f6260257f1e37407
moritz-hauser <hausermoritz@protonmail.com>
2025-02-13 11:01:02 +0100

    update clear-downloads.sh

--- clear-downloads.sh
+++ clear-downloads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+	
 # -------- FUNCTIONS ----------
 # Print usage in case of misinput
 usage() {
@@ -17,13 +17,14 @@ downloads_status() {
 }
 # Ask for permission for each file individually, then remove them
 clear_and_ask() {
-	# Durch Dateien iterieren
+	# Iterate folder
 	for file in "$HOME/Downloads"/*; do
-		# If file does not exist (Downloads is emtpy) -> continue
+		# If not a file -> continue
 		[ -e "$file" ] || continue
+		[ -f "$file" ] || continue
 		echo "Datei: $(basename "$file")"
-		# Nach Erlaubnis fragen
-		read -p "Datei löschen? (y/n) " choice
+		# Ask permission
+		read -p "Remove file? (y/n) " choice
 		if [ "$choice" = "y" ]; then 
 			rm -v -- "$file"
 		fi
@@ -32,13 +33,60 @@ clear_and_ask() {
 }
 # Remove all duplicate files in the folder
 clear_duplicates() {
-	# TODO 
-	echo "clear_duplicates called"
+	# Declare associative array (key: hash, value: file)
+	declare -A file_hashes
+	# Iterate folder
+	for file in "$HOME/Downloads"/*; do
+		# If not a file -> continue
+		[ -e "$file" ] || continue
+		[ -f "$file" ] || continue
+		# Get hash of file
+		hash=$(sha256sum "$file" | awk '{print $1}')
+        	if [[ -n "${file_hashes[$hash]}" ]]; then
+        		# Array already contains hash -> delete file
+            		rm -v -- "$file"
+        	else
+        		# Array does not contain hash -> add hash
+            		file_hashes[$hash]="$file"
+        	fi
+        done
+	echo "---"
 }
 # Print list, then ask which files *not* to delete
 clear_and_ask_list() {
-	# TODO
-	echo "clear_and_ask_list called"
+	# 1. Create array containg all files
+	declare -a files=()
+	for file in "$HOME/Downloads"/*; do
+		# If not a file -> continue
+		[ -e "$file" ] || continue
+		[ -f "$file" ] || continue
+		# Add file to array
+		files+=("$file")
+	done
+	# 2. Print array (with indexes)
+	echo "Found ${#files[@]} files:"
+	for i in "${!files[@]}"; do
+        	echo "[$i] $(basename "${files[$i]}")"
+    	done
+	# 3. Read input
+	echo -e "\nInput indexes of files to keep."
+    	echo "Separated by spaces. Example: 2 4 13"
+    	read -p "Indexes to keep: " -a indexes_to_keep
+	# 4. Iterate files and delete those not listed
+    	for file_index in "${!files[@]}"; do
+    		# Check whether user wants to keep this file
+        	keep=false
+        	for index_keep in "${indexes_to_keep[@]}"; do
+            		if [[ "$index_keep" == "$file_index" ]]; then
+                		keep=true
+                		break
+            		fi
+        	done
+        	# Delete file
+        	if [ "$keep" = false ]; then
+            		rm -v -- "${files[$file_index]}"
+		fi
+    	done
 }
 
 # ----------- LOGIC -----------
@@ -52,6 +100,12 @@ fi
 if [ $# -gt 1 ]; then
 	usage
 	exit 1
+fi
+
+# Check if Downloads is empty
+if [ -z "$(find "$HOME/Downloads" -type f)" ]; then
+    echo "Downloads does not contain any files."
+    exit 0
 fi
 
 # Handle arguments
